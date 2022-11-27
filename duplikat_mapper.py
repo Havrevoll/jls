81ddc01b35ff3b6d73e36d49452ed56b07808287
Ola Haugen Havrevoll <havrevoll@gmail.com>
2022-11-27 22:13:02 +0100

    leika meg litt

--- duplikat_mapper.py
+++ duplikat_mapper.py
@@ -32,10 +32,10 @@ def md5_update_from_dir(directory, hash):
 def md5_dir(directory):
     return md5_update_from_dir(directory, hashlib.md5()).hexdigest()
 
+if __name__ == '__main__':
+    src = Path("/mnt/o/arkiver/")
+    mapper = md5_dir(src)
 
-src = Path("/mnt/o/arkiver/")
-mapper = md5_dir(src)
-
-for md5,path in mapper.items():
-    if len(path)> 1:
-        print(path)
+    for md5,path in mapper.items():
+        if len(path)> 1:
+            print(path)
